860b91f312041191d74af0c5c50f279255c1ad2f
Cesar Arley Ojeda Escobar <mas.arley2009@hotmail.com>
2019-08-26 09:48:21 -0500

    Modulo 01 - Mejorado el formateo de salida

--- modulos/modulo_01/codigoFuente/principal.py
+++ modulos/modulo_01/codigoFuente/principal.py
@@ -10,7 +10,8 @@ def main():
         cadena = str(input("Escribe una cadena: "))
     else:
         # Valido para python 2.x
-        cadena = str(raw_input("Escribe una cadena: "))
+        # cadena = str(raw_input("Escribe una cadena: "))
+        pass
     # Verificamos que la cadena no este vacia
     if len(cadena) <= 0:
         print(cadena + " No es una cadena valida")
@@ -19,15 +20,17 @@ def main():
         analizador = AnalizadorLexico(cadena)
 
         # Imprimimos unos titulos para mejor entendimiento
-        print("\n\nResultado del analisis lexico:")
-        print("\nSimbolo\t\t\t\tTipo\t\t\t\tCodigo de Tipo")
+        print("\n\n")
+        print("Resultado del analisis lexico:")
+        print('{:*<75}'.format(""))
+        print('{:<30}'.format("Simbolo") + '{:<30}'.format("Tipo") + '{:<5}'.format("Codigo de Tipo"))
 
         # Realizamos un ciclo que continue hasta que el simobolo sea un $
         while analizador.caracter != "$":
             # Analizamos el primer simbolo de la cadena
             analizador.siguienteSimbolo()
             # Imprimimos el simbolo junto al tipo de cadena
-            print(analizador.simbolo + "\t\t\t\t" + analizador.tipoCadena(analizador.tipo) + "\t\t\t\t" + str(analizador.tipo))
+            print('{:<30}'.format(analizador.simbolo) + '{:<30}'.format(analizador.tipoCadena(analizador.tipo)) + '{:<5}'.format(str(analizador.tipo)))
     # Fin del if-else
 
 if __name__ == '__main__':
